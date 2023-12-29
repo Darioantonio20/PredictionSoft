2c90d3f6e5311a210ee6c1eca28bba0f9f6cda2d
Darío Antonio Gutiérrez Álvarez <105560538+Darioantonio20@users.noreply.github.com>
2023-12-29 15:17:06 -0600

    [ADD] Carrousel & Section Cards

--- PredictionSoft/src/pages/LandingPage.jsx
+++ PredictionSoft/src/pages/LandingPage.jsx
@@ -1,9 +1,13 @@
 import NavBar from "../components/atoms/NavBar";
+import SectionCards from "../components/molecules/SectionCards";
+import Carousel from "../components/atoms/Carrousel";
 
 function LandingPage() {
     return ( 
         <>
             <NavBar/>
+            <Carousel/>
+            <SectionCards/>
         </>
      );
 }
--- README.md
+++
@@ -1,3 +0,0 @@
-# PredictionSoft
-# Dev : Darío Antonio Gutiérrez Álvarez
-Rediseño de la página Prediction Soft con las tecnologias de React js, Css, Bootstrap entre otra libreria para el UI
