affda98958c54e13e41c19722cae366d277cf518
DarioAntonio <antoniodario10@hotmail.com>
2023-12-29 21:00:39 -0600

    [ADD] reloader

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,15 +1,35 @@
-import NavBar from "../components/atoms/NavBar";
-import SectionCards from "../components/molecules/SectionCards";
-import Carousel from "../components/atoms/Carrousel";
+import React, { useState, useEffect } from 'react';
+import NavBar from '../components/atoms/NavBar';
+import SectionCards from '../components/molecules/SectionCards';
+import Carousel from '../components/atoms/Carrousel';
+import '../assets/style/LandingPage.css';
 
 function LandingPage() {
-    return ( 
+    const [loading, setLoading] = useState(true);
+    
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setLoading(false);
+        }, 2258);
+        return () => clearTimeout(timer);
+    }, []);
+
+    return (
         <>
-            <NavBar/>
-            <Carousel/>
-            <SectionCards/>
+            {loading ? (
+                <div className="containercito align-items-center">
+                    <div className="loadercito mb-5 mt-5"></div>
+                    <div className="loader mt-5"></div>
+                </div>
+            ) : (
+                <>
+                    <NavBar />
+                    <Carousel />
+                    <SectionCards />
+                </>
+            )}
         </>
-     );
+    );
 }
 
 export default LandingPage;
