79970bc036234e15f9a5e108c50db9f43f930319
Darío Antonio Gutiérrez Álvarez <105560538+Darioantonio20@users.noreply.github.com>
2024-01-03 19:48:56 -0600

    [FIX] text blue

--- src/components/molecules/Section2.jsx
+++ src/components/molecules/Section2.jsx
@@ -17,16 +17,16 @@ function Section2() {
                         </div>
                     </div>
                 </div>
-                <div className="text-center mt-5 mb-3">
-                    <h2>¿POR QUE ELEGIR PREDICTION?</h2>
+                <div className="bg-info p-0 pt-2 pb-2">
+                    <h1 className="text-center text-light">¿POR QUE ELEGIR PREDICTION?</h1>
                 </div>
-                    <div className="container-fluid p-0">
-                        <div className="row justify-content-center">
-                            <div className="col-md-12 p-0">
-                                <img src={Img3Section2} loading="lazy" className="img-fluid" style={{ width: '100%' }} alt="Imagen 2 Section 2" />
-                            </div>
+                <div className="container-fluid p-0 ">
+                    <div className="row justify-content-center">
+                        <div className="col-md-12 p-0">
+                            <img src={Img3Section2} loading="lazy" className="img-fluid" style={{ width: '100%' }} alt="Imagen 2 Section 2" />
                         </div>
                     </div>
+                </div>
                 <div className="bg-info p-0 pt-2 pb-2">
                     <h1 className="text-center text-light">NOSOTROS</h1>
                 </div>
