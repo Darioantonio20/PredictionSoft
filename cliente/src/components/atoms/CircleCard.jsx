d88f57257fbe5aa56ea33b18c2e0fea32c2e6341
Darío Antonio Gutiérrez Álvarez <105560538+Darioantonio20@users.noreply.github.com>
2024-01-22 20:37:23 -0600

    [FIX] imgs

--- cliente/src/components/atoms/CircleCard.jsx
+++ cliente/src/components/atoms/CircleCard.jsx
@@ -1,13 +1,12 @@
-/* The code you provided is a React component called `CircleCard`. */
+/* The code you provided is a React component called CircleCard. */
 import ImgSection2 from "../../assets/img/imgHeroPrediction.png";
-import "../../assets/style/CircleCard.css";
 
 function CircleCard() {
     return (
         <>
-            <div className="container-fluid">
+           <div className="container-fluid">
                 <div className="row justify-content-center">
-                    <div className="col-md-12 mt-1">
+                    <div className="col-md-12 mt-5 mb-5">
                         <img
                             src={ImgSection2}
                             loading="lazy"
@@ -21,4 +20,4 @@ function CircleCard() {
     );
 }
 
-export default CircleCard;
+export default CircleCard;
--- cliente/src/components/atoms/Nosotros.jsx
+++ cliente/src/components/atoms/Nosotros.jsx
@@ -1,8 +1,3 @@
-/**
- * The "Nosotros" function is a React component that renders a section about "us" with a heading and an
- * image.
- * @returns The component `Nosotros` is being returned.
- */
 import ImgNosotros from "../../assets/img/imgNosotros.jpg";
 
 function Nosotros() {
@@ -12,9 +7,9 @@ function Nosotros() {
             <div className="bg-info p-0 pt-2 pb-2">
                 <h1 className="text-center text-light animate__animated animate__bounceInDown">NOSOTROS</h1>
             </div>
-            <div className="container-fluid p-0">
+            <div className="container-fluid p-0 mt-2 mb-2">
                 <div className="row justify-content-center">
-                    <div className="col-md-12">
+                    <div className="col-md-12 text-center d-flex justify-content-center align-items-center">
                         <img src={ImgNosotros} loading="lazy" className="img-fluid" alt="Imagen Nosotros" />
                     </div>
                 </div>
